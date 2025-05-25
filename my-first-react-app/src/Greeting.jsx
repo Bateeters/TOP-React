73e4f9f4ab0519a5c4ec921ab4a22ed0659b758a
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-05-25 14:20:24 -0400

    first commit

---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,5 @@
+function HelloWorld() {
+    return <h1>Hello and welcome to my first React component!</h1>
+}
+
+export default HelloWorld;
---
+++ my-first-react-app/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import HelloWorld from './Greeting.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelloWorld />
+  </StrictMode>,
+)
