f4afacea81f89cc94dbfe68a864da9f20b6d3078
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-05-25 14:56:20 -0400

    Created NewComponent and fixed errors

---
+++ my-first-react-app/src/NewComponent.jsx
@@ -0,0 +1,15 @@
+function NewComponent(){
+    return(
+        <div>
+            <h1>Test title for NewComponent</h1>
+            <svg>
+                <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+            </svg>
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default NewComponent;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import HelloWorld from './Greeting.jsx'
+import NewComponent from './NewComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelloWorld />
+    <NewComponent />
   </StrictMode>,
 )
